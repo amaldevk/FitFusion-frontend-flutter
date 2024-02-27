dafbf913c7e332b477962e66155cfa87649c30ac
Akshaykj2000 <akshaykj2000@gmail.com>
2024-02-27 11:39:19 +0530

    master

---
+++ android/app/src/main/kotlin/com/example/fitfusion_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fitfusion_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:fitfusion_app/pages/userLogin.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(home());
+}
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
+
+
