04b2705a706f47b0c7a173643e4ee096a5b0a1da
Akshaykj2000 <akshaykj2000@gmail.com>
2024-02-28 19:36:20 +0530

    add

--- lib/pages/userLogin.dart
+++ lib/pages/userLogin.dart
@@ -32,7 +32,7 @@ class _LoginState extends State<Login> {
       preferences.setString("userid", userId);
       print("successfull uid"+userId);
 
-     // Navigator.push(context,MaterialPageRoute(builder: (context)=>SelectPackagePage()));
+     Navigator.push(context,MaterialPageRoute(builder: (context)=>SelectPackagePage()));
     }
 
     else if(response["status"]=="Invalid user"){
