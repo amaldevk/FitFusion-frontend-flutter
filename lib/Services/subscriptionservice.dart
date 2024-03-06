5b418dcc6022c9a9609c678ff87a3f258f1cbb9a
ar7unms <msarjun077@gmail.com>
2024-03-06 21:51:11 +0530

    hh

---
+++ lib/Services/subscriptionservice.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+import 'package:fitfusion_app/Models/PackageModel.dart';
+import 'package:http/http.dart' as http;
+
+class UpdateApiService {
+  Future<dynamic> addPackageApi(String userId, String newPackageId,String Token) async
+  {
+    var client =http.Client();
+    var apiUrl= Uri.parse("http://localhost:3006/api/subscription/update");
+
+    final response =await client.post(apiUrl,
+        headers: <String,String>{
+          'Content-Type' : 'application/json',
+          'Authorization' : 'Bearer $Token'
+        },
+        body: jsonEncode(<String,String>{
+          "userId": userId,
+          "newPackageId": newPackageId
+        })
+    );
+    if(response.statusCode==200)
+    {
+      return json.decode(response.body);
+    }
+    else
+    {
+      throw Exception("failed to add");
+    }
+  }
+}
